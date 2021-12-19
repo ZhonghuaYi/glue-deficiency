e009fb68219ed23773b10e54a9c4016ea70f6500
ZhonghuaYi <yizhonghua6@gmail.com>
2021-12-19 20:39:33 +0800

    feat: 8：39PM, 12/19/2021.

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from matplotlib import pyplot as plt
 
 import Spatial
 
-REFER_LIST = ['test000.bmp']
-DEFECT_LIST = ['test001.bmp', 'test002.bmp']
+REFER_LIST = ['test000.bmp']  # 参考图像
+DEFECT_LIST = ['test001.bmp', 'test002.bmp']  # 缺陷图像
+PRE_AREA_NUM = 12
 
 
 def get_histogram(in_pic, scale=256):
@@ -120,11 +121,12 @@ def threshold_segment(img, threshold):
     return out
 
 
-def area_segment(img):
+def area_segment(img, pre_area_num):
     """
     图像的区域分割
+    :param pre_area_num: 初始假设的区域数量，应略多于实际区域数量
     :param img: 输入图像
-    :return: 区域面积， 区域起始坐标
+    :return: （区域数值，区域面积）(ndarray)， 区域起始坐标
     """
 
     def neighbor_expand(x, y, value, region_area):
@@ -147,7 +149,7 @@ def area_segment(img):
                 region_area = neighbor_expand(x1, y1, value, region_area)
         return region_area
 
-    area_value = (20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240)
+    area_value = np.linspace(start=2, stop=255, num=pre_area_num, dtype=np.uint8)
     area_num = 0
     regions = []
     area_start = []
@@ -155,25 +157,22 @@ def area_segment(img):
     for i in range(img_shape[0]):
         for j in range(img_shape[1]):
             if img[i][j] == 0:
-                area = 0
+                area = [0, 0]  # 区域的数值与面积
                 area_start.append((i, j))
-                area = neighbor_expand(i, j, area_value[area_num], area)
+                area[1] = neighbor_expand(i, j, area_value[area_num], area[1])
+                area[0] = area_value[area_num]
                 area_num += 1
                 regions.append(area)
 
-    return regions, area_start
+    return np.array(regions), area_start
 
 
 if __name__ == '__main__':
-    sample = sample_generate()
-    count = 1
-    structure_element = cv.getStructuringElement(cv.MORPH_RECT, (7, 7))
+    sample = sample_generate()  # 样本的生成器
+    count = 1  # 图像的计数
+    structure_element = cv.getStructuringElement(cv.MORPH_RECT, (7, 7))  # 用于形态学计算的矩形结构元素
+    target_region_areas = []
     for image in sample:
-
-        if count != 3:
-            count += 1
-            continue
-
         '''
         在参考图像中，当手动阈值在37时，阈值分割效果明显。于是考虑到灰度小于37的区域大概面积占比是0.3，
         于是将图像中灰度值较低的30%区域分割出来。这里利用了cdf，它本身是直方图的累积分布，因此只需要寻找
@@ -182,7 +181,7 @@ if __name__ == '__main__':
         hist = get_histogram(image)
         img_cdf = cdf(hist) / 255.
         index = (np.abs(img_cdf - 0.3)).argmin()  # index即为阈值
-        print("阈值：" + str(index))
+        # print("阈值：" + str(index))
 
         '''
         得到阈值后，对图像进行阈值分割，然后对不规则区域应用中值滤波平滑。
@@ -203,22 +202,35 @@ if __name__ == '__main__':
         del compare
 
         '''
-        将图像分成若干个区域，之后再将目标区域分割出来
+        将图像分成若干个区域
         '''
-        areas, areas_start = area_segment(image)
-        invalid_area_start = np.where(np.array(areas) == 1)[0].tolist()
+        areas, areas_start = area_segment(image, PRE_AREA_NUM)
+
+        '''
+        当区域面积为1时，该区域为无效区域，将该区域置为背景，即清除该区域
+        '''
+        invalid_area_start = np.where(areas[:, 1] == 1)[0].tolist()
         for i in invalid_area_start:
             ind = areas_start[i]
             image[ind[0]][ind[1]] = 255
 
-        # image = threshold_segment(image, (60, 61))
-        print(areas)
-        print(areas_start)
-        print(invalid_area_start)
+        '''
+        将面积第二的区域提取出来
+        '''
+        ind = np.argsort(areas[:, 1])[-2]
+        target_area_value = areas[ind, 0]  # 获取到面积第二的区域的值
+        target_region_areas.append(areas[ind, 1])  # 将该区域的面积记录下来
+        compare = np.ones(image.shape, dtype=image.dtype) * target_area_value
+        image = np.array(image != compare).astype(image.dtype) * 255  # 将数值为target_area_value的区域分离出来
+
+        # print(areas)
+        # print(areas_start)
+        # print(invalid_area_start)
 
         window_name = 'img' + str(count)
         cv.imshow(window_name, image)
         count += 1
 
+    print(target_region_areas)  # 输出区域的面积
     cv.waitKey(0)
 
